34a6750830d4cf1de26c0c8902b33fd63ea4ae55
Pavan-D4 <pavand@gmail.com>
2024-10-01 05:47:51 +0000

    auto commit Tue Oct  1 05:47:51 UTC 2024

---
+++ jenkins-auto-backup.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#
+
+sudo cp /var/lib/jenkins/ /home/ec2-user/jenkins-backup
+
+cd /home/ec2-user/jenkins-backup
+
+cd ../
+
+chown -R ec2-user jenkins-backup
+
+cd jenkins-backup
+
+git add .
+
+git commit -m " auto commit $(date) "
+
+git push https://$1@github.com/Pavan-D4/jenkins-backup.git master
